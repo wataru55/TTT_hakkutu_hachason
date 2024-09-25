1521e034082e84c7a2cfdce31e57d50a6a8d1099
Ukyo <ukyou20011121@gmail.com>
2024-09-26 01:45:02 +0900

    add: スチームの初期段階実装

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,41 +5,30 @@ import Image from 'next/image';
 export default function Display() {
 
   return (
-    <div className='flex flex-col h-screen w-full'>
-      <div className='flex items-end h-[15%]'>
-        <h1 className='text-7xl ml-5'>最高の整いを</h1>
+    <div className='relative flex flex-col justify-center items-center h-screen w-full px-10'>
+      <div className='h-[30%] w-full border-b border-zinc-600'>
+
       </div>
-      <div className='flex h-[70%]'>
-        <div className='flex flex-col justify-end h-full w-1/2'>
-          <div className='relative flex flex-col justify-center items-center h-[80%]'>
-            <Link href="/reserve">
-              <Image
-                src="/reserve.jpeg"
-                alt='reserve'
-                width={600}
-                height={400}
-              />
-            </Link>
-            <p className='text-3xl mb-2'>Reserve</p>         
-          </div>
+       <div className='flex flex-col justify-center p-2 h-[40%]'>
+        <div className='text-center mb-10'>
+          <h1 className='text-6xl mb-5 font-serif'>最高の整いを</h1>
+          <h1 className='text-3xl font-serif'>究極のリラックス体験をあなたに</h1>
         </div>
-        <div className='flex flex-col justify-start h-full w-1/2'>
-          <div className='relative flex flex-col justify-center items-center h-[80%]'>
-            <Link href="/display">
-              <Image
-                src="/display.jpg"
-                alt='reserve'
-                width={600}
-                height={400}
-                className=''
-              />
-            </Link>           
-            <p className='text-3xl mt-2'>Display</p>
-          </div>
+        <div className='flex justify-center p-2 gap-10'>
+          <Link href="/reserve" className='bg-white rounded-md px-5 py-3 text-black hover:bg-slate-200 font-serif font-medium'>
+            予約する
+          </Link>
+          <Link href="/display" className='bg-white rounded-md px-5 py-3 text-black hover:bg-slate-200 font-serif font-medium'>
+            画面を見る
+          </Link>
         </div>
-      </div>
-      <div className='h-[15%]'>
-        <h1 className='text-right text-7xl mr-5'>体験しませんか</h1>
+       </div>
+       <div className='h-[30%] w-full border-t border-zinc-600'>
+        <div className='absolute top-0 left-0 w-full h-full overflow-hidden pointer-events-none z-10'>
+          <p className='steam-01'>
+            <img src="/steam.svg" alt="steam" />
+          </p>
+        </div>
       </div>
     </div>  
   );
