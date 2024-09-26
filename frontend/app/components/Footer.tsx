804954eecb807683908f93de83fec86bf98a4475
vivy_phtela <tubasa.0405@icloud.com>
2024-09-26 10:31:01 +0900

    add: フッターを追加

---
+++ frontend/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="text-center text-gray-400 text-sm">
+      <p>&copy; 2024 Hackz hackathon TTT. All rights reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
